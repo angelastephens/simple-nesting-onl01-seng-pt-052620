5f07e7dfc748d27f89e8946f9a23b3f5f94ccbd7
Angela Stephens <angelastephens317@gmail.com>
2020-06-07 14:44:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
- 		      :known_for => "Ruby",
-          :languages => ["LISP", "C"] 
-  }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,7 +80,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     
+     }
+
+  programmer_hash [:yukihiro_matsumoto = {:known_for => "Ruby",
+    :languages => ["LISP", "C"] 
   }
 
   programmer_hash
